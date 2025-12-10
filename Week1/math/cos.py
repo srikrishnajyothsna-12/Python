63298b0caa0167070c2caa0781c3278e36aaae99
srikrishnajyothsna-12 <srikrishnajyothsna@gmail.com>
2025-12-10 22:28:14 +0530

    Add files via upload

--- Week1/math/cos.py
+++ Week1/math/cos.py
@@ -2,4 +2,4 @@ import math
 angle = int(input("Enter an angle between base and hypotenuse: "))
 hypotenuse = int(input("Enter the hypotenuse of right angle triangle in meters: "))
 base = hypotenuse * math.cos(math.radians(angle))
-print("The lenght of base of triangle is: ",round(base,2))
+print("The length of base of triangle is: ",round(base,2))
--- Week1/math/sin.py
+++ Week1/math/sin.py
@@ -2,4 +2,4 @@ import math
 angle = int(input("Enter an angle between base and hypotenuse: "))
 hypotenuse = int(input("Enter the hypotenuse of right angle triangle(in meters): "))
 height = hypotenuse * math.sin(math.radians(angle))
-print("The lenght of height of triangle is: ",round(height,2))
+print("The length of height of triangle is: ",round(height,2))

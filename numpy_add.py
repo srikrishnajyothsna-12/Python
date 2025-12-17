697069b341b3faec6bdfa3542b0147e6f3b00d30
srikrishnajyothsna-12 <srikrishnajyothsna@gmail.com>
2025-12-17 18:10:42 +0530

    Add files via upload

---
+++ numpy_add.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr1 = np.array([2,12,30,5])
+arr2 = np.array([4,24,11,25])
+print("Array 1: ",arr1)
+print("Array 2: ",arr2)
+result = np.add(arr1,arr2)
+print("Resultant Array: ",result)
